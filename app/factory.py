f880d91586f6091cbee77827f21bff9ea6014a4e
Lucino772 <lucapalmi772@gmail.com>
2024-02-06 17:15:46 +0100

    feat: added simple hello world page

--- app/factory.py
+++ app/factory.py
@@ -1,11 +1,16 @@
 import jinja_partials
-from flask import Flask
+from flask import Flask, render_template
 
 from app.extensions import htmx
+
+
+def index():
+    return render_template("index.html")
 
 
 def create_app():
     app = Flask(__name__)
     jinja_partials.register_extensions(app)
     htmx.init_app(app)
+    app.add_url_rule("/", view_func=index)
     return app
