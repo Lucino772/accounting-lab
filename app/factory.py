05acf490451afc4bcc43e9c631626dc3eef86e93
Lucino772 <lucapalmi772@gmail.com>
2024-02-06 17:00:23 +0100

    chore(deps): added dependencies to work with htmx and tailwindcss

--- app/factory.py
+++ app/factory.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import jinja_partials
+from app.extensions import htmx
 
 
 def create_app():
     app = Flask(__name__)
+    jinja_partials.register_extensions(app)
+    htmx.init_app(app)
     return app
